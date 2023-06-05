35df39485a688098a67378eaadcbd2dc7b2a1074
MYT <dimitriettr@gmail.com>
2023-06-05 13:39:08 +0300

    Add 'clean-solution' script

---
+++ src/clean-solution.bat
@@ -0,0 +1,16 @@
+rmdir /s /q .\Ace.CSharp.StructuredAutoMapper\bin
+rmdir /s /q .\Ace.CSharp.StructuredAutoMapper\obj
+
+rmdir /s /q .\Ace.CSharp.StructuredAutoMapper.Abstractions\bin
+rmdir /s /q .\Ace.CSharp.StructuredAutoMapper.Abstractions\obj
+
+rmdir /s /q .\Ace.CSharp.StructuredAutoMapper.Abstractions.Test\bin
+rmdir /s /q .\Ace.CSharp.StructuredAutoMapper.Abstractions.Test\obj
+
+rmdir /s /q .\sample\Ace.CSharp.StructuredAutoMapper.Sample\bin
+rmdir /s /q .\sample\Ace.CSharp.StructuredAutoMapper.Sample\obj
+
+rmdir /s /q .\sample\Ace.CSharp.StructuredAutoMapper.Sample.Tests\bin
+rmdir /s /q .\sample\Ace.CSharp.StructuredAutoMapper.Sample.Tests\obj
+
+pause
